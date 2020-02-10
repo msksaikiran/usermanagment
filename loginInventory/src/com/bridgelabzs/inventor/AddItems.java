af121e9e0f1bd4682825b9551d45a559d911462f
msksaikiran <msaikiran0728@gmail.com>
2020-02-10 19:20:23 +0530

    [sai] add. login using the sevlets

---
+++ loginInventory/src/com/bridgelabzs/inventor/AddItems.java
@@ -0,0 +1,22 @@
+package com.bridgelabzs.inventor;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import com.bridgelabzs.Utility.Util;
+
+public class AddItems extends HttpServlet {
+	private JSONObject json1 = new JSONObject();
+	private JSONArray list = new JSONArray();
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+
+		
+	}
+}
---
+++ loginInventory/src/com/bridgelabzs/inventor/Products.java
@@ -0,0 +1,40 @@
+package com.bridgelabzs.inventor;
+
+import java.util.Set;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import com.bridgelabzs.Utility.Util;
+
+@WebServlet("/products")
+public class Products extends HttpServlet{
+
+	public void post(HttpServletRequest req,HttpServletResponse res) {
+		inventoryProducts();
+	}
+
+@SuppressWarnings("unchecked")
+public void inventoryProducts() {
+	
+	JSONArray obj;
+	try {
+		obj = Util.readFile();
+		obj.forEach((data) -> {
+			JSONObject data2 = (JSONObject) data;
+			Set keys = data2.keySet();
+			System.out.print(String.join("", keys) + "\t");
+
+		});
+		System.out.println();
+	} catch (Exception e) {
+		System.out.println("Not aProper File");
+	}
+
+}
+}
